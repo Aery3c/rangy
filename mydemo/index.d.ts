5c36f7d8fc083b8d6021dbd11552af24e13030f7
Aery <294725131@qq.com>
2022-01-05 19:26:19 +0800

    Update index.d.ts

--- mydemo/index.d.ts
+++ mydemo/index.d.ts
@@ -16,9 +16,42 @@ interface RangeIteratorCallback {
 }
 
 interface dom {
+  /**
+   * 返回一个布尔值, 如果node是文本节点 返回true 否则false
+   * @param node
+   * @return {boolean}
+   */
   isCharacterDataNode(node: Node): boolean;
+  /**
+   * 根据index分割文本节点.
+   *
+   * clone 这个文本节点 remove cloneNode 前半部分(0 - index), remove souceNode 后半部分(index - sourceNode.length - 1).
+   *
+   * insertAfert(cloneNode, sourceNode).
+   * @param node source node.
+   * @param index 分割参考的边界点.
+   * @return cloneNode
+   */
   splitDataNode(node: Node, index: number): Node;
+
+  /**
+   * 在precedingNode的后面插入insertNode.
+   * @param insertNode 被插入的节点.
+   * @param precedingNode 前面的那个节点.
+   */
   insertAfert(insertNode: Node, precedingNode: Node): Node;
+
+  /**
+   * 迭代RangeIterator对象, 每一次迭代的结果将通过回调函数透出.
+   *
+   * 在每一次的迭代返回的结果中. 如果node是一个被部分选中的节点, 创建一个子RangeIterator对象, 重新递归iterateSubtree.
+   *
+   * 子RangeIterator对象: cloneRange当前range,
+   *
+   * 部分选中概念: node是一个元素节点并且它是range.startContainer或者range.endContainer的祖先元素, 因为在开头我们采用了splitDataNode的做法, 因此节点内的文本节点被一分为二.
+   * @param iterator RangeIterator的实例对象.
+   * @param callbackfn 每一次迭代完成后执行的回调函数.
+   */
   iterateSubtree(iterator: RangeIterator, callbackfn: RangeIteratorCallback): void;
   getClosestAncestorIn(node: Node, ancestor: Node, selfIsAncestor: boolean): Node | null;
   inspectNode(node: Node): string;
