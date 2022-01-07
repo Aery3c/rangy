caa1766e6293fa8a8e4c1205e7d4aea13558955a
Aery <294725131@qq.com>
2022-01-07 17:37:37 +0800

    Update index.d.ts

--- mydemo/index.d.ts
+++ mydemo/index.d.ts
@@ -1,11 +1,12 @@
 interface Range {
+
   /**
-   * 从range首尾选择的边界开始分割文本节点, 只会处理首尾节点是文本节点的情况.
+   * 从range首尾(sc, ec)选择的边界开始分割文本节点, 如果sc,ec是元素节点则忽略不处理, 如果是文本节点 则分割
    *
    * 首尾具有相同节点 范围中的文本节点将会是一个新的节点并分离.
    *
    * 首尾是不同节点 首尾范围中的文本节点将会是一个新的节点并分离.
-   * @return [sc, ec]
+   * @return [sc, ec] 分离出的新的首尾文本节点
    */
   splitRangeBoundaries(): Node[];
 
@@ -21,19 +22,40 @@ interface Range {
   setStartAndEnd(): void;
 
   /**
-   * 更新range的边界, 内部实用setStart和setEnd.
+   * 更新range的边界, 内部使用setStart和setEnd.
    * @param sc
    * @param so
    * @param ec
    * @param eo
    */
   updateBoundaries(sc: Node, so: number, ec: Node, eo: number): void;
+
+  /**
+   * 从范围上获取node节点.
+   * @param nodeTypes 获取的节点类型 一个数组 数组外的类型将全部被过滤掉.
+   * @param filter 过滤函数 返回一个布尔值 如果为false则过滤掉.
+   */
   getNodes(nodeTypes: number[], filter?: (node: Node) => boolean): Node[];
+
+  highlight(className?: string, options?: {}): void;
 }
 
 interface RangeIterator {
+  /**
+   * 将指针移到下一个节点 返回当前这个节点 直到null为止.
+   */
   next(): Node | null,
+
+  /**
+   * 判断节点是否被部分选中.
+   *
+   * 部分选中概念: 部分选中的节点只会出现在范围跨标签且节点必须肯定是首尾任意节点的父元素节点.
+   */
   isPartiallySelectedSubtree(): boolean;
+
+  /**
+   *
+   */
   getSubtreeIterator(): RangeIterator;
 }
 
@@ -87,7 +109,33 @@ interface dom {
    * @param selfIsAncestor 如果是true 那么从node开始 false则从node.parent开始
    */
   getClosestAncestorIn(node: Node, ancestor: Node, selfIsAncestor: boolean): Node | null;
+
+  /**
+   * 在控制台中检阅当前node节点.
+   *
+   * 破碎的节点输出: [Broken node].
+   *
+   * 文本节点输出: "文本".
+   *
+   * 元素节点输出: <nodeName id>[index: 节点在容器中的位置, length: 节点的长度, 通过子节点得到][node.innerHTML].
+   *
+   * 其余节点输出: node.nodeName
+   * @param node 当前节点
+   */
   inspectNode(node: Node): string;
+
+  /**
+   * 返回节点是否是一个破碎的节点.
+   *
+   * 破碎的节点是指 运行node.parent会抛出异常的节点.
+   * @param node
+   */
+  isBrokenNode(node: Node): boolean;
+
+  /**
+   * 获取当前节点在当前容器中的位置.
+   * @param node
+   */
   getNodeIndex(node: Node): number;
 
   /**
@@ -104,6 +152,15 @@ interface dom {
    * @param selfIsAncestor 如果true 从descendant开始 否则从descendant.parent开始
    */
   isAncestorOf(ancestor: Node, descendant: Node, selfIsAncestor: boolean): boolean;
+
+  /**
+   * 返回节点自身的长度
+   *
+   * 文本节点: 返回自身文本的长度
+   *
+   * 元素节点: 返回子节点的长度
+   * @param node
+   */
   getNodeLength(node: Node): number;
   gEBI(id: string): Node;
 }
@@ -113,18 +170,16 @@ interface Window {
   dom: dom;
 }
 
-// interface domrange {
-//   RangeIterator: RangeIterator;
-//   util: {
-//     isNonTextPartiallySelected(node: Node, range: Range): boolean;
-//   }
-// }
-
-// declare var domrange: domrange;
-
 declare var dom: dom
 
 declare var RangeIterator: {
   prototype: RangeIterator;
+  /**
+   * 创建RangeIterator实例
+   *
+   * 在构造函数中 我们会去初始化迭代对象分配好迭代的起始(_first, _last)位置
+   * @param range
+   * @param clonePartiallySelectedTextNodes
+   */
   new(range: Range, clonePartiallySelectedTextNodes: boolean): RangeIterator
 }
