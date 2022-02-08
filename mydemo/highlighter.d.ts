d7acfdb6e30042f0bdc26c492404d00c4bf7bc7e
Aery <294725131@qq.com>
2022-02-08 17:08:42 +0800

    Update highlighter.d.ts

--- mydemo/highlighter.d.ts
+++ mydemo/highlighter.d.ts
@@ -1,11 +1,5 @@
 export interface Highlighter {
   /**
-   * 用荧光笔在文本节点上涂抹
-   * @param textNode
-   */
-  applyToTextNode(textNode: CharacterData): void;
-
-  /**
    * 用荧光笔涂抹range.
    *
    * @param range
@@ -13,11 +7,24 @@ export interface Highlighter {
   apply(range: Range): Highlighter;
 }
 
+interface Mark {
+  /**
+   * 用荧光笔在文本节点上涂抹
+   * @param textNode
+   */
+  applyToTextNode(textNode: CharacterData): void;
+}
+
 interface Options {
 
 }
 
+declare var Mark: {
+  prototype: Mark;
+  new(className: string): Mark;
+}
+
 declare var Highlighter: {
   prototype: Highlighter;
-  new(className: string, options: Options): Highlighter;
+  new(name: string, options: Options): Highlighter;
 }
