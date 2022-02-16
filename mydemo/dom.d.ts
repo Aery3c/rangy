35e416f570d67105b0c85e906d251bb921e4c02d
Aery <294725131@qq.com>
2022-02-16 18:20:39 +0800

    Update dom.d.ts

--- mydemo/dom.d.ts
+++ mydemo/dom.d.ts
@@ -1,190 +1,93 @@
-import {RangeIterator, RangeIteratorCallback} from "./index";
+interface Range {
 
-interface dom {
   /**
-   * 返回一个布尔值, 如果node是文本节点 返回true 否则false
-   * @param node
-   * @return {boolean}
-   */
-  isCharacterDataNode(node: Node): boolean;
-  /**
-   * 根据index分割文本节点.
+   * 从range首尾(sc, ec)选择的边界开始分割文本节点, 如果sc,ec是元素节点则忽略不处理, 如果是文本节点 则分割
    *
-   * clone 这个文本节点 remove cloneNode 前半部分(0 - index), remove souceNode 后半部分(index - sourceNode.length - 1).
+   * 首尾具有相同节点 范围中的文本节点将会是一个新的节点并分离.
    *
-   * insertAfert(cloneNode, sourceNode).
-   * @param node source node.
-   * @param index 分割参考的边界点.
-   * @return cloneNode
+   * 首尾是不同节点 首尾范围中的文本节点将会是一个新的节点并分离.
    */
-  splitDataNode(node: Node, index: number): Node;
+  splitRangeBoundaries(): void;
 
   /**
-   * 在precedingNode的后面插入insertNode.
-   * @param insertNode 被插入的节点.
-   * @param precedingNode 前面的那个节点.
-   */
-  insertAfert(insertNode: Node, precedingNode: Node): Node;
-
-  /**
-   * 迭代RangeIterator对象, 每一次迭代的结果将通过回调函数透出.
+   * 设置range的边界(start和end).
    *
-   * 在每一次的迭代返回的结果中. 如果node是一个被部分选中的节点, 创建一个子RangeIterator对象, 重新递归iterateSubtree.
+   * 函数内部获取arguments对象. 至少传3个参数 sc = args[0] ec = args[1].
    *
-   * 子RangeIterator对象: cloneRange当前range,
+   * case args.lengh = 3, so = args[2], eo = args[2].
    *
-   * 部分选中概念: node是一个元素节点并且它是range.startContainer或者range.endContainer的祖先元素, 因为在开头我们采用了splitDataNode的做法, 因此节点内的文本节点被一分为二.
-   * @param iterator RangeIterator的实例对象.
-   * @param callbackfn 每一次迭代完成后执行的回调函数.
+   * case args.length = 4, so = args[2], eo = args[3].
    */
-  iterateSubtree(iterator: RangeIterator, callbackfn: RangeIteratorCallback): void;
+  setStartAndEnd(): void;
 
   /**
-   * 返回node最接近ancestor的祖先元素.
-   * @param node 当前节点
-   * @param ancestor 祖先节点
-   * @param selfIsAncestor 如果是true 那么从node开始 false则从node.parent开始
+   * 更新range的边界, 内部使用setStart和setEnd.
+   * @param sc
+   * @param so
+   * @param ec
+   * @param eo
    */
-  getClosestAncestorIn(node: Node, ancestor: Node, selfIsAncestor: boolean): Node | null;
+  updateBoundaries(sc: Node, so: number, ec: Node, eo: number): void;
 
   /**
-   * 在控制台中检阅当前node节点.
+   * 从范围上获取node节点.
+   * @param nodeTypes 获取的节点类型 一个数组 数组外的类型将全部被过滤掉.
+   * @param filter 过滤函数 返回一个布尔值 如果为false则过滤掉.
+   */
+  getNodes<T>(nodeTypes?: number[], filter?: (node: T) => boolean): T[];
+
+  /**
    *
-   * 破碎的节点输出: [Broken node].
+   * @param sourceRange
+   */
+  intersection(sourceRange: Range): void;
+
+  /**
    *
-   * 文本节点输出: "文本".
-   *
-   * 元素节点输出: <nodeName id>[index: 节点在容器中的位置, length: 节点的长度, 通过子节点得到][node.innerHTML].
-   *
-   * 其余节点输出: node.nodeName
-   * @param node 当前节点
+   * @param sourceRange
    */
-  inspectNode(node: Node): string;
+  intersectsRange(sourceRange: Range): boolean;
 
   /**
-   * 返回节点是否是一个破碎的节点.
-   *
-   * 破碎的节点是指 运行node.parent会抛出异常的节点.
-   * @param node
+   * 检阅range
    */
-  isBrokenNode(node: Node): boolean;
+  inspect(console?: boolean): string;
 
-  /**
-   * 获取当前节点在当前容器中的位置.
-   * @param node
-   */
-  getNodeIndex(node: Node): number;
-
-  /**
-   * 返回布尔值, 如果ancestor是descendant是祖先节点, 返回true, 否则返回false.
-   * @param ancestor 祖先节点.
-   * @param descendant 后代节点.
-   */
-  isOrIsAncestorOf(ancestor: Node, descendant: Node): boolean;
-
-  /**
-   * 跟isOrIsAncestorOf相同 但多了第三个参数是一个布尔值 指示是从descendant还是descendant.parent开始
-   * @param ancestor 祖先节点
-   * @param descendant 后代节点
-   * @param selfIsAncestor 如果true 从descendant开始 否则从descendant.parent开始
-   */
-  isAncestorOf(ancestor: Node, descendant: Node, selfIsAncestor: boolean): boolean;
-
-  /**
-   * 返回节点自身的长度
-   *
-   * 文本节点: 返回自身文本的长度
-   *
-   * 元素节点: 返回子节点的长度
-   * @param node
-   */
-  getNodeLength(node: Node): number;
-  gEBI(id: string): Node;
-
-  /**
-   * 比较范围的边界点.
-   * @param nodeA
-   * @param offsetA
-   * @param nodeB
-   * @param offsetB
-   */
-  comparePoints(nodeA: Node, offsetA: number, nodeB: Node, offsetB: number): number;
-
-  /**
-   * 如果node的祖先节点包含className, 返回ancestor, 否则返回null.
-   * @param node
-   * @param className
-   */
-  getSelfOrAncestorWithClass(node: Node, className: string): Node | null;
-
-  /**
-   * 如果node包含class, 返回true, 否则false.
-   * @param el
-   * @param className
-   */
-  hasClass(el: Element, className: string): boolean;
-
-  /**
-   * el add className.
-   * @param el
-   * @param className
-   */
-  addClass(el: Element, className: string): void;
-
-  /**
-   * fullClassName 是否包含 className
-   * @param fullClassName
-   * @param className
-   */
-  classNameContainsClass(fullClassName: string, className: string): boolean;
-
-  /**
-   * 两个元素是否具有相同的class.
-   * @param el1
-   * @param el2
-   */
-  haveSameClasses(el1: Element, el2: Element): boolean;
-
-  /**
-   * 将元素的class排序后返回
-   * @param className
-   */
-  sortClassName(className: string): string;
-
-  /**
-   * 获取元素的class
-   * @param el
-   */
-  getClass(el: Element): string;
-
-  getComputedStyleProperty(el: Element, propName: string): string;
-
-  /**
-   * 对比el1 和 el2 的属性节点
-   *
-   * attributes的长度不同 返回false
-   *
-   * Attr.specified不同 返回false
-   *
-   * Attr.nodeValue不同 返回false
-   * @param el1
-   * @param el2
-   */
-  elementsHaveSameNonClassAttributes(el1: Element, el2: Element): boolean;
-
-  /**
-   * 从dom中删除这个节点 并返回被删除的这个节点
-   *
-   * @param node
-   */
-  removeNode(node: Node): Node;
-
-  /**
-   * 获取node的祖先元素 如果祖先元素的class等于className
-   * @param node
-   * @param className
-   */
-  getSelfOrAncestorWithClass(node: Node, className: string): Node | null;
 }
 
-declare var dom: dom
+interface Tinter {
+  /**
+   * 将class应用到此范围.
+   */
+  applyToRange(range: Range): void;
+
+  /**
+   * 如果此范围已被class着色, 那么返回true, 否则fasle.
+   */
+  isTinterToRange(range: Range): boolean;
+
+  /**
+   * 从node开始, 遍历向上查询包含这个class的父节点并返回, 完成时没有找到返回null.
+   */
+  getSelfOrAncestorWithClass<T extends Node>(node: T): T | null;
+
+  /**
+   * 如果node包含class返回true, 否则返回false.
+   */
+  hasClass<T extends Node>(node: T): boolean;
+}
+
+interface RangeIterator {}
+
+interface Util {
+  isRangeValid(): boolean;
+  getNodesInRange<T extends Node>(range: Range, nodeTypes?: number[], filter?: (node: T) => boolean): T[];
+}
+
+interface Aery {
+  createTinter(className: string, options: {}): Tinter;
+  createRangeIterator(range: Range): RangeIterator;
+  util: Util
+}
+
+declare var aery: Aery;
