41d2836326f27f1f57fcc219f332b8ff12dfd738
Aery <294725131@qq.com>
2022-02-22 17:46:19 +0800

    Update test.js

--- mydemo/test.js
+++ mydemo/test.js
@@ -4,5 +4,18 @@ document.querySelector('#test').addEventListener('click', function() {
 });
 
 document.querySelector('#function-test').addEventListener('click', function() {
+  const tinter = aery.createTinter('highlight', {});
+  const range = window.getSelection().getRangeAt(0);
+  const textNodes = range.getNodes([Node.TEXT_NODE]);
+
+  let ancestorWithClass;
+
+
+
+  textNodes.forEach(function(textNode) {
+    ancestorWithClass = tinter.getSelfOrAncestorWithClass(textNode);
+    if (ancestorWithClass) {
+    }
+  });
 
 });
